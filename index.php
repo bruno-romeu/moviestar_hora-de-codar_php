5c5f87ff86ea741047ddcd542bd70c57676bb9e9
Bruno <bruno.rsilva2004@gmail.com>
2025-04-09 23:14:29 -0300

    implementação do front-end da página principal do site, e lógica para buscar imagem e nome do banco de dados para mostrar no client side.

--- index.php
+++ index.php
@@ -1,9 +1,27 @@
 <?php
-    require_once('templates/header.php')
+    require_once('templates/header.php');
+    require_once('dao/movieDAO.php');
+
+    $movieDAO = new MovieDAO($conn, $BASE_URL);
+
+    $latestMovies = $movieDAO->getLatestMovies();
+    $actionMovies = [];
+    $comedyMovies = [];
 ?>
 
     <div id="main-container" class="container-fluid">
-        <h1>Corpo do site</h1>
+        <h2 class="section-title">Filmes novos</h2>
+        <p class="section-description">Veja as críticas dos últimos filmes adicionados no MovieStar</p>
+        <div class="movies-container">
+
+        <?php foreach($latestMovies as  $movie): ?>
+            <?php require('templates/movie_card.php'); ?>
+        <?php endforeach; ?>
+
+        <h2 class="section-title">Ação</h2>
+        <p class="section-description">Veja os melhores filmes de ação</p>
+        <h2 class="section-title">Comédia</h2>
+        <p class="section-description">Veja os melhores filmes de comédia</p>
     </div>
 
 <?php
